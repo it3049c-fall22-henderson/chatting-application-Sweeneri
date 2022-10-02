3d206206318746a023410fc33dea53d0e0088a4e
dev <dev@localgeotours.com>
2022-10-02 17:35:22 -0400

    add listener to save username and en/disable input

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,19 +1,38 @@
 const nameInput = document.getElementById(`my-name-input`);
 const myMessage = document.getElementById(`my-message`);
 const sendButton = document.getElementById(`send-button`);
+const saveButton = document.getElementById(`save-button`);
 const chatBox = document.getElementById(`chat`);
 const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
 const MILLISECONDS_IN_TEN_SECONDS = 10000;
 
 setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
 
+if(localStorage.getItem(`username`)) {
+  nameInput.value = localStorage.getItem(`username`);
+  sendButton.disabled = false;
+  myMessage.disabled = false;
+} else {
+  sendButton.disabled = true;
+  myMessage.disabled = true;// setAttribute(`disabled`, `true`);
+}
+
 sendButton.addEventListener(`click`, function(sendButtonClickEvent) {
   sendButtonClickEvent.preventDefault();
-  const sender = nameInput.value;
+  const sender = localStorage.getItem(`username`);
   const message = myMessage.value;
 
   sendMessages(sender,message);
   myMessage.value = ``;
+});
+saveButton.addEventListener(`click`, function(saveEvent) {
+  // eslint-disable-next-line no-debugger
+  debugger;
+  saveEvent.preventDefault();
+  const sender = nameInput.value;
+  localStorage.setItem(`username`, sender);
+  sendButton.disabled = false;
+  myMessage.disabled = false;
 });
 
 function fetchMessages() {
